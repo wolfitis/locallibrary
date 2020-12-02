0e83425a0295f103382f7326759563a57f0e5807
wolfitis <webblog123@gmail.com>
2020-12-02 05:29:10 -0500

    added inline editing and sectioning of records

--- catalog/admin.py
+++ catalog/admin.py
@@ -12,10 +12,16 @@ admin.site.register(Genre)
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
 	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+	fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
+
+# Inline editing of associated records
+class BookInstanceInline(admin.TabularInline):
+	model = BookInstance
+	extra = 0
 
 # Now to create and register the new models; 
 # for the purpose of this demonstration, we'll instead use the @register decorator to register the models (this does exactly the same thing as the admin.site.register() syntax):
@@ -28,9 +34,21 @@ display_genre
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
 	list_display = ('title', 'author', 'display_genre')
+	inlines = [BookInstanceInline]
+
 
 # register the admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-	pass
+	list_filter = ('status', 'due_back')
+
+	# sectioning detail view
+	fieldsets = (
+		(None, {
+			'fields': ('book', 'imprint', 'id')
+		}),
+		('Availability', {
+			'fields': ('status', 'due_back')
+		}),
+	)
 
