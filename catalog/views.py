8e2ed166e0ec57e4f87af094cb315f164459bd14
wolfitis <webblog123@gmail.com>
2020-12-09 03:20:07 -0500

    sessions enabled

--- catalog/views.py
+++ catalog/views.py
@@ -12,11 +12,16 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
+    # getting visits counts with sessions
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
